98a5e5220917ea54710d2a76a2ecbc176a9e3c32
buddika-iresh <buddikairesh427@gmail.com>
2025-06-24 03:20:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PREFIX: ".", // Command prefix
-  OWNER_NUMBER: "94771234567", // Replace with your number (no + sign)
-  SESSION_ID: "abcDEFghiJkLmnopQRStuvWXYZ1234567890", // MEGA.nz File ID only (without whole link)
+  OWNER_NUMBER: "94721551183", // Replace with your number (no + sign)
+  SESSION_ID: "Zq0wFBzS#5T_Rm0JCr5duBvnOe1BsBR4v9iseaeAajjF2m_K_PNI", // MEGA.nz File ID only (without whole link)
   MODE: "public", // Options: "public", "private", "inbox"
   BUTTONS_ON: true, // Enable/disable buttons by default
   AUTO_STATUS_SEEN: true, // Automatically view WhatsApp statuses
