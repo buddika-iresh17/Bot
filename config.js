e7d6ae10ec187b70532d09afd8c03f1b41dd30ca
buddika-iresh <buddikairesh427@gmail.com>
2025-06-23 22:40:01 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  PREFIX: ".",               // Bot commands prefix
+  PREFIX: ".",               // Bot command prefix
   MODE: "public",            // Bot mode (public/private)
-  OWNER_NUM: "94721551183",  // Owner phone number with country code (without +)
-  SESSION_ID: "szVhzB5Q#YZM90WNczC-TLS2tYtpmkQU5Htc-bBbMM4JU0-empyk",  // Your mega.nz session id for creds.json download
-  AUTO_STATUS_SEEN: true,    // true to enable auto view statuses feature
-
-  // Optional flags if you want to use here to toggle features globally
-  ANTI_LINK_ENABLED: true,   // true to enable antilink feature globally by default (can override per group)
-  ANTI_DELETE_ENABLED: true  // true to enable antidelete feature globally by default (can override per group)
+  OWNER_NUM: "94721551183",  // Owner phone number (with country code, no +)
+  SESSION_ID: "szVhzB5Q#YZM90WNczC-TLS2tYtpmkQU5Htc-bBbMM4JU0-empyk",  // Mega.nz session ID for auth file download
+  AUTO_STATUS_SEEN: true,    // true to auto view statuses
+  ANTI_LINK_ENABLED: true,   // true to enable AntiLink by default
+  ANTI_DELETE_ENABLED: true  // true to enable AntiDelete by default
 };
