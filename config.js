f41e4a015db643990ddf356e13c9fbd1c00f1330
buddika-iresh <buddikairesh427@gmail.com>
2025-06-26 02:11:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "Zq0wFBzS#5T_Rm0JCr5duBvnOe1BsBR4v9iseaeAajjF2m_K_PNI",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "94721551183"
-    BOT_MODE: process.env.BOT_MODE || "public",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "94721551183",
     PREFIX: process.env.PREFIX || ".",
     MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|decd54b4fa030634e54d6c87fdffbb95f0bb9fb5",
     };
