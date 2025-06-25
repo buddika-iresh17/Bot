80cd4b2bdd9d118a10a3651b885b749e08b231d9
buddika-iresh <buddikairesh427@gmail.com>
2025-06-25 21:57:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "Zq0wFBzS#5T_Rm0JCr5duBvnOe1BsBR4v9iseaeAajjF2m_K_PNI",
     MODE: process.env.MODE || "private",
     PREFIX: process.env.PREFIX || "."
     };
