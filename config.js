383e406e6b92a859e963c4e243b3e643b2cd16f1
buddika-iresh <buddikairesh427@gmail.com>
2025-06-25 14:55:55 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -7,11 +7,17 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "Zq0wFBzS#5T_Rm0JCr5duBvnOe1BsBR4v9iseaeAajjF2m_K_PNI",
-    MODE: process.env.MODE || "public",
+    MODE: process.env.MODE || "private",
     PREFIX: process.env.PREFIX || ".",
     AUTO_REACT: process.env.AUTO_REACT || "false",
+    ANTI_LINK: process.env.ANTI_LINK || "false",
+    ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
     READ_MESSAGE: process.env.READ_MESSAGE || "false",
-    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-    MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|decd54b4fa030634e54d6c87fdffbb95f0bb9fb5"
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/vbi10j.png",
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyBP9qAGQUHjtIPuaZcyvSnbZDGSyHUD6bc",
+    MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|decd54b4fa030634e54d6c87fdffbb95f0bb9fb5",
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39" // omdbapi.com
     };
+    
