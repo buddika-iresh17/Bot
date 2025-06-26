8aaf60ed0912892157ba732c8b4cea6cc58846f8
buddika-iresh <buddikairesh427@gmail.com>
2025-06-26 15:49:59 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -9,6 +9,10 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "VykXxIqT#hQhWQdmK_chRK82q0-jXlVdM3ufVM8T-i4JEA9UbnAI",
     MODE: process.env.MODE || "public",
     PREFIX: process.env.PREFIX || ".",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
     MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|decd54b4fa030634e54d6c87fdffbb95f0bb9fb5",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/vbi10j.png",
     };
     
